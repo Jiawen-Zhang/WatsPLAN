fd04e9a30607cd711d5252eecee8b3ee53c53daf
Wenjiu Wang <vanjor1014@gmail.com>
2020-05-21 15:51:15 -0400

    First commit

---
+++ app/src/main/java/com/wwjz/watsplan/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.wwjz.watsplan
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            val intent = Intent()
+            intent.setClass(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+            finish()
+        }, 1000)
+
+    }
+}
